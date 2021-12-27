3b52037e250456e79b2a216ca8044bff80f0b52b
zooeymoon1989 <kimmi-moon@163.com>
2021-12-27 22:59:23 +0800

    add quick sort and test pass

--- basicSort/quickSort/quickSort.go
+++ basicSort/quickSort/quickSort.go
@@ -11,12 +11,16 @@ func quickSort(a []int) []int {
 	}
 	p := pivot(len(a))
 	a[0], a[p] = a[p], a[0]
-	i := 1
-	j := len(a) - 1
-	for i < j {
-
+	var left []int
+	var right []int
+	for i := 1; i < len(a); i++ {
+		if a[i] > a[0] {
+			right = append(right, a[i])
+		} else {
+			left = append(left, a[i])
+		}
 	}
-	return a
+	return append(quickSort(append(left, a[0])), quickSort(right)...)
 }
 
 func pivot(n int) int {
---
+++ basicSort/quickSort/quickSort_test.go
@@ -0,0 +1,15 @@
+package quickSort
+
+import (
+	"fmt"
+	"interviewPractice/basicSort/sortData"
+	"reflect"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	if !reflect.DeepEqual(quickSort(sortData.Data), sortData.Expect) {
+		fmt.Printf("%+v\n", sortData.Data)
+		t.Errorf("this two are not the same")
+	}
+}
