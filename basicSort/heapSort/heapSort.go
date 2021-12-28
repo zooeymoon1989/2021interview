69d3158a1f2b7e7ea2702608be839008e3ff9775
zooeymoon1989 <liwenqiang@pwrd.com>
2021-12-28 17:26:24 +0800

    update heap sort and test pass

--- basicSort/heapSort/heapSort.go
+++ basicSort/heapSort/heapSort.go
@@ -1,29 +1,26 @@
 package heapSort
 
-import "fmt"
-
 func heapSort(a []int) []int {
-
-	for i := 0; i < len(a)/2+1; i++ {
-		heapSlice(a, i)
-	}
-
+	heapSlice(a, 0)
 	for i := len(a) - 1; i >= 0; i-- {
 		a[0], a[i] = a[i], a[0]
 		heapSlice(a[:i], 0)
 	}
-	fmt.Printf("%+v\n", a)
 	return a
 }
 
 func heapSlice(a []int, offset int) {
 	if offset*2+1 < len(a) && a[offset] <= a[offset*2+1] {
 		a[offset], a[offset*2+1] = a[offset*2+1], a[offset]
-		heapSlice(a, offset*2+1)
+	} else {
+		return
 	}
 
 	if offset*2+2 < len(a) && a[offset] <= a[offset*2+2] {
 		a[offset], a[offset*2+2] = a[offset*2+2], a[offset]
-		heapSlice(a, offset*2+2)
+	} else {
+		return
 	}
+	heapSlice(a, offset*2+1)
+	heapSlice(a, offset*2+2)
 }
